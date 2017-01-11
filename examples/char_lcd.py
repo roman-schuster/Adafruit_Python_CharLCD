03f81bc85979459e1f03a8680cc6f1484d2cd8bc
Roman Schuster <schuster@sas.upenn.edu>
2017-01-11 18:48:31 -0500

    Update char_lcd.py

--- examples/char_lcd.py
+++ examples/char_lcd.py
@@ -14,7 +14,7 @@ lcd_d7        = 12
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
-lcd_rows    = 2
+lcd_rows    = 1
 
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
